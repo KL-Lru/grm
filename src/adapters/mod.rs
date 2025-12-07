71c3c14e919474032e9d7305d3cc4297db71e1e6
KL-Lru <26233827+KL-Lru@users.noreply.github.com>
2025-12-08 01:10:25 +0900

    fix mod

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -1,9 +1,7 @@
-pub mod ports;
-
 pub mod git_cli;
-pub mod unix_fs;
 pub mod terminal_interaction;
+pub mod unix_fs;
 
 pub use git_cli::GitCli;
-pub use unix_fs::UnixFs;
 pub use terminal_interaction::TerminalInteraction;
+pub use unix_fs::UnixFs;
